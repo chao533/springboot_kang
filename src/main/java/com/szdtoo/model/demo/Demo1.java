6e6a44c52ab80848ac9b9ea69087665b5469c201
ck <chao533@qq.com>
2020-01-09 20:27:11 +0800

    测试代码

--- src/main/java/com/szdtoo/model/demo/Demo1.java
+++ src/main/java/com/szdtoo/model/demo/Demo1.java
@@ -1,15 +1,17 @@
 package com.szdtoo.model.demo;
 
 import java.text.SimpleDateFormat;
+import java.util.concurrent.Callable;
 
 public class Demo1 {
 
 	
 	public static void main(String[] args) throws Exception {
-		ThreadLocal<String> local = new ThreadLocal<>();
-		local.set("123");
-		String string = local.get();
-		System.out.println(Thread.currentThread().getName());
+//		ThreadLocal<String> local = new ThreadLocal<>();
+//		local.set("123");
+//		String string = local.get();
+//		System.out.println(Thread.currentThread().getName());
+		test2();
 	}
 	
 	private static ThreadLocal<SimpleDateFormat> format1 = new ThreadLocal<SimpleDateFormat>() {
@@ -19,5 +21,22 @@ public class Demo1 {
 			return super.initialValue();
 		}
 	};
+	
+	public void test() {
+		new Callable<String>() {
+
+			@Override
+			public String call() throws Exception {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
+	
+	public static void test2() throws Exception {
+		Callable<String> call = () -> "21";
+		String s = call.call();
+		System.out.println(s);
+	}
 	
 }
