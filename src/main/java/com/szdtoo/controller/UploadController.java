41ae7c31f39ac8964c0b2ac6eccf40f66fc26b5b
Administrator <Administrator@PC-201906261744>
2020-01-13 14:29:27 +0800

    提交

--- src/main/java/com/szdtoo/controller/UploadController.java
+++ src/main/java/com/szdtoo/controller/UploadController.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +16,6 @@ import com.szdtoo.common.msg.Message;
 import com.szdtoo.common.utils.FdfsClientUtils;
 import com.szdtoo.model.param.ImgCutParam;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
