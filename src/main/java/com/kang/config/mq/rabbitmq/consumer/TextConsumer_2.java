2c728ca783ca5ec5b58f029502fc766febee4ce0
ck <chao533@qq.com>
2020-05-07 20:00:24 +0800

    edit

--- src/main/java/com/kang/config/mq/rabbitmq/consumer/TextConsumer_2.java
+++ src/main/java/com/kang/config/mq/rabbitmq/consumer/TextConsumer_2.java
@@ -4,9 +4,6 @@ import java.text.MessageFormat;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
